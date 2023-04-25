7551670fddf95ca273a8492c698f0be3748f842e
BohBOhTN <Baha.etc@gmail.com>
2023-04-25 11:25:09 +0200

    Creation de la table PRODUIT

--- Exercice1.sql
+++ Exercice1.sql
@@ -3,3 +3,11 @@ CREATE TABLE REPRESENTANT (
     NomRep VARCHAR(20),
     Adresse VARCHAR(20)
 );
+
+CREATE TABLE PRODUIT (
+    NumProd number PRIMARY KEY,
+    NomProd VARCHAR(20),
+    Couleur VARCHAR(20),
+    Poids DECIMAL(7, 2),
+    Categorie VARCHAR(20)
+);
